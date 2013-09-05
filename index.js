445fc484fd96f73626de4de022f4ee233493d72b
Dustin Higginbotham <dustinh052@gmail.com>
2013-09-04 23:12:18 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,212 @@
+var fs     = require('fs'),
+	http   = require('http');
+
+var version = '0.0.1';
+
+var findImageType = function(buffer) {
+	var int32View = new Int32Array(buffer);
+	switch(int32View[0]) {
+		case 137: 
+			return "image/png";
+		case 255:
+			return "image/jpg";
+		default:
+			throw '6px: Unexpected file type!'
+	}
+};
+
+var toDataURI = function(buffer) {
+	return 'data:'+ findImageType(buffer) + ';base64,' + buffer.toString('base64');
+};
+
+var parseInput = function(input, cb) {
+
+	if (typeof input == 'string') {
+		fs.readFile(input, function (err, data) {
+			if (err) throw err;
+			cb(toDataURI(data));
+		});
+	}
+
+	if (Buffer.isBuffer(input)) {
+		cb(toDataURI(input));
+	}
+};
+
+var sendToServer = function(data, fn) {
+
+	var content = JSON.stringify(data),
+		contentLength = content.length;
+
+	var options = {
+		host: 'api.6px.io',
+		port: 80,
+		path: '/users/' + px.userData.userId + '/jobs',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': contentLength
+		}
+	};
+
+	var response = [];
+	var req = http.request(options, function(res) {
+
+		res.setEncoding('utf8');
+
+		res.on('data', function (chunk) {
+			response.push(chunk);
+		});
+
+		res.on('end', function() {
+			fn(response.join(''));
+		});
+
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	// write data to request body
+	req.write(content);
+	req.end();
+};
+
+var _6px = function(input) {
+	this.image = input;
+	this.actions = {};
+};
+
+/**
+ * Resize input
+ *
+ * @param {object} size: Pass in width and/or height
+ */
+_6px.prototype.resize = function(size) {
+
+	this.actions.resize = size;
+
+	return this;
+};
+
+_6px.prototype.filter = function(type, value) {
+
+	if (!this.actions.filter) {
+		this.actions.filter = {};
+	}
+
+	// User took a shortcut and used an object to define them all at once
+	if (typeof type == 'object') {
+		this.actions.filter = type;
+		return this;
+	}
+
+	this.actions.filter[type] = value;
+
+	return this;
+};
+
+_6px.prototype.priority = function(value) {
+
+	this.priority = value;
+
+	return this;
+
+};
+
+_6px.prototype.rotate = function(options) {
+
+	this.actions.rotate = options;
+
+	return this;
+};
+
+_6px.prototype.crop = function(position) {
+	
+	this.actions.crop = position;
+
+	return this;
+};
+
+_6px.prototype.save = function(options, fn) {
+	
+	var _this = this;
+
+	if (typeof options == 'function') {
+		var fn = options;
+		var options = {};
+	}
+
+	var json = {
+		callback: {
+			url: options.callback || null
+		},
+		priority: (this.priority || 0),
+		user_id: px.userData.userId,
+		output: [{
+			ref: [0],
+			tag: options.tag || null,
+			type: options.type || 'image/png',
+			// url: options.url || null,
+			methods: [this.actions]
+		}]
+	};
+
+
+	parseInput(this.image, function(data) {
+		
+		json.input = [];
+		json.input.push(data);
+
+		sendToServer(json,
+			function(res) {
+				fn(null, res);
+			},
+			function() {
+				fn(true);
+			});
+
+	});
+
+};
+
+var px = function(input) {
+
+	if (!px.userData) {
+		throw '6px: You must call init!';
+	}
+
+	return new _6px(input);
+};
+
+px.version = version;
+
+px.priorities = {
+	critical: 4,
+	high: 3,
+	medium: 2,
+	normal: 1
+};
+
+/**
+ * Use this to set up your account with apiKey, etc
+ */
+px.init = function(data) {
+
+	if (!data.apiKey) {
+		throw '6px: apiKey is required!';
+	}
+
+	if (!data.apiSecret) {
+		throw '6px: apiSecret is required!';
+	}
+
+	if (!data.userId) {
+		throw '6px: userId is required!';
+	}
+
+	px.userData = data;
+};
+
+module.exports.px = px;
